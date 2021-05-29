e594c76bc98fe80bfad296fb958d6574ac90708f
algmoreno <alg.moreno00@gmail.com>
2021-05-28 22:14:48 -0700

    making validation for answers

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,54 +2,65 @@ var scoreCounter= 0;
 
 var myQuiz = [
     {
-    question: "What is the capital of California?",
-    choices: ["Los Angeles", "San Francisco", "Sacramento", "San Diego", "Oakland"],
-    correctAnswer: "Sacramento"
+    question: "What color is the sky?",
+    choices: ["Green", "Blue", "Red", "Yellow", "Orange"],
+    correctAnswer: "Blue"
     }, 
     {
-    question: "What is the capital of Pennsylvania?",
-    choices: ["Pittsburgh", "Philadelphia", "Harrisburg", "Erie"],
-    correctAnswer: "Harrisburg"
+    question: "What city are you in?",
+    choices: ["Phoenix", "Los Angeles", "Tucson", "Chicago"],
+    correctAnswer: "Tucson"
     }, 
     {
-    question: "What is the capital of Florida?",
-    choices: ["Tallahassee", "Tampa", "Miami", "Jacksonville"],
-    correctAnswer: "Tallahassee"
+    question: "How many hours in a day?",
+    choices: ["24", "34", "23", "19"],
+    correctAnswer: "24"
     },
     {
-    question: "What is the capital of Georgia?",
-    choices: ["Augusta", "Atlanta", "Savannah"],
-    correctAnswer: "Atlanta"
+    question: "What language are you using?",
+    choices: ["HTML", "JavaScript", "CSS"],
+    correctAnswer: "JavaScript"
     }
 ]; 
  
- var questionIndex = -1; 
+var result = myQuiz.filter(obj => {
+    console.log(obj.correctAnswer); 
+    
+})
+
+var questionIndex = -1; 
+ 
 
 function nextQuestion() {
     document.body.innerHTML = '';
-    ++questionIndex;
+    ++questionIndex; 
     document.write(myQuiz[questionIndex].question + "<br />");
 
         for (var i=0; i < myQuiz[questionIndex].choices.length; i++) {
-        document.write("<input type=radio id=myRadio name=radAnswer>" + myQuiz[questionIndex].choices[i] + "<br />");
+            var radioButtons = document.createElement("input");
+            radioButtons.type = "radio"; 
+            radioButtons.addEventListener("click", showResults); 
+        // document.write("<input type=radio id=myRadio name=radAnswer>" + myQuiz[questionIndex].choices[i] + "<br />");
         }
     
-    if ()
+
     if (questionIndex < (myQuiz.length -1 )) {
         var nextButton = document.createElement("input");
         nextButton.type = "button";
         nextButton.value = "Next question";
-        nextButton.addEventListener('click', nextQuestion);
+        nextButton.addEventListener("click", nextQuestion);
         document.body.appendChild(nextButton);
     }
 
-    else if (questionIndex < (myQuiz.length)) {
+    else {
         var submitButton = document.createElement("input");
         submitButton.type = "button";
         submitButton.value = "Submit";
         submitButton.addEventListener("click", showResults);
         document.body.appendChild(submitButton); 
     }
+
+
 
 };
 
@@ -58,5 +69,3 @@ function showResults() {
 }
 
 nextQuestion(); 
-    
-
