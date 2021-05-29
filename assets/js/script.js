5e3ced14cc7227b2c987162711fa36fec23528ea
algmoreno <alg.moreno00@gmail.com>
2021-05-29 13:05:44 -0700

    answer validation working

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,10 @@
 var quizContainer = document.querySelector("#quizContainer"); 
 var scoreCounter= 0; 
 
+var scoreCount = function () {
+    
+}
+
 var myQuiz = [
     {
     question: "What color is the sky?",
@@ -23,15 +27,16 @@ var myQuiz = [
     correctAnswer: "JavaScript"
     }
 ]; 
- 
-var result = myQuiz.filter(obj => {
-    return obj.correctAnswer; 
-    
-})
+
 
 var questionIndex = -1; 
 
 function start() {
+    var userInput = document.createElement("input");
+    userInput.type = "text";
+    userInput.placeholder = "Enter your initials here"
+    quizContainer.appendChild(userInput); 
+    
     var startButton = document.createElement("input");
     startButton.type = "button";
     startButton.value = "Begin!";
@@ -42,23 +47,42 @@ function start() {
 
 
 function nextQuestion() {
-    document.body.innerHTML = "";
+    quizContainer.innerHTML = ""; 
     ++questionIndex; 
-    document.write(myQuiz[questionIndex].question + "<br />");
+    var question = myQuiz[questionIndex].question 
+    var divEl = document.createElement("div");
+    divEl.setAttribute ("id", "questionsDiv")
+    var pEl = document.createElement("p");
+    pEl.textContent = question; 
+    divEl.append(pEl); 
+    
 
         for (var i=0; i < myQuiz[questionIndex].choices.length; i++) {
-            // var radioButton = document.createElement("input");
-            // radioButton.type = "radio"; 
-            // radioButton.addEventListener("click", ); 
-        document.write("<input type=radio id=myRadio name=userAnswer>" + myQuiz[questionIndex].choices[i] + "<br />");
+            var radioButton = document.createElement("input");
+            radioButton.setAttribute ("type","radio"); 
+            radioButton.setAttribute ("name", "userAnswer");
+            radioButton.setAttribute ("class", "question");
+            radioButton.setAttribute ("value", myQuiz[questionIndex].choices[i]);
+            radioButton.setAttribute ("id", myQuiz[questionIndex].choices[i]);
+        
+
+            var label = document.createElement("label")
+            label.setAttribute ("for", myQuiz[questionIndex].choices[i]);
+            label.textContent = myQuiz[questionIndex].choices[i]; 
+            
+            var br = document.createElement("br")
+            label.append(br); 
+            divEl.append(radioButton);
+            divEl.append(label); 
         }
     
+        quizContainer.append(divEl);
 
     if (questionIndex < (myQuiz.length -1 )) {
         var nextButton = document.createElement("input");
         nextButton.type = "button";
         nextButton.value = "Next question";
-        nextButton.addEventListener("click", nextQuestion);
+        nextButton.addEventListener("click", checkAnswer);
         quizContainer.appendChild(nextButton);
     }
 
@@ -77,16 +101,31 @@ function showResults() {
 
 }
 
-function checkAnswer() {
-    if ("userAnswer" === result) {
-        console.log(result);
+function checkAnswer(event) {
+    var userAnswer 
+    var questionsDivEl = document.querySelectorAll(".question");
+
+    for (i=0; i<questionsDivEl.length; i++) {
+        var elementCheck = questionsDivEl[i].checked;
+        var element = questionsDivEl[i].value; 
+
+        if (elementCheck === true) {
+            userAnswer = element 
+        }
+    }
+
+
+    if (userAnswer === myQuiz[questionIndex].correctAnswer) {
+        nextQuestion(); 
+        scoreCounter +5; 
     }
 
     else {
-        
+        console.log(false);
     }
 
-    // nextQuestion();
+
+   
 }
 
 start();
